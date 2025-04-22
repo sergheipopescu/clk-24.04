5ddcb2db63f0e8ef6ba6cb8366f49c3a099d82dc
Serghei Popescu <sp@clickwork.ro>
2025-04-22 17:38:47 +0300

    First phase of progress mask

--- make-clk.sh
+++ make-clk.sh
@@ -2,7 +2,7 @@
 
 set -a													# export all variables
 
-scriptdir=$(pwd)											# set script directory
+scriptdir=$(dirname "$0")										# set script directory
 
 if [[ $# -eq 0 ]]; then											# if no argument
 	echo
@@ -18,96 +18,112 @@ fi
 ###################
 
 # Set timezone and 24h clock
-sudo timedatectl set-timezone Europe/Bucharest
-sudo update-locale 'LC_TIME="C.UTF-8"'
+timedatectl set-timezone Europe/Bucharest
+update-locale 'LC_TIME="C.UTF-8"'
 
 # Set hostname
-sudo hostnamectl set-hostname "$fqdn"
+hostnamectl set-hostname "$fqdn"
 
 # Install packages for customization and cleanup unneeded packages
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confold" upgrade
-sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
-sudo apt-get install mc nano libwww-perl haveged fortune-mod software-properties-common dirmngr apt-transport-https argon2 -y
-sudo apt-get --no-install-recommends -y install landscape-common
-sudo apt-get remove ufw -y
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confold" upgrade
+DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
+apt-get install mc nano libwww-perl haveged fortune-mod software-properties-common dirmngr apt-transport-https argon2 -y
+apt-get --no-install-recommends -y install landscape-common
+apt-get remove ufw -y
 
 # Change ssh port
-sudo sed -i 's|#Port 22|Port 2282|' /etc/ssh/sshd_config
+sed -i 's|#Port 22|Port 2282|' /etc/ssh/sshd_config
 
 # Allow password authentication
-sudo sed -i 's|PasswordAuthentication no|PasswordAuthentication yes|' /etc/ssh/sshd_config
+sed -i 's|PasswordAuthentication no|PasswordAuthentication yes|' /etc/ssh/sshd_config
 
 # Disable password authentication for root only
-sudo sed -i "/#MaxAuthTries/c\MaxAuthTries	3" /etc/ssh/sshd_config
-sudo sed -i 's|PermitRootLogin yes|PermitRootLogin prohibit-password|' /etc/ssh/sshd_config
-sudo sed -i "\$a\\\nMatch User root\n	PasswordAuthentication no" /etc/ssh/sshd_config
+sed -i "/#MaxAuthTries/c\MaxAuthTries	3" /etc/ssh/sshd_config
+sed -i 's|PermitRootLogin yes|PermitRootLogin prohibit-password|' /etc/ssh/sshd_config
+sed -i "\$a\\\nMatch User root\n	PasswordAuthentication no" /etc/ssh/sshd_config
 
 # motd cleanup
-sudo sed -i 's|ENABLED=1|ENABLED=0|' /etc/default/motd-news
-sudo sed -i '/Graph this data/d' /usr/lib/python3/dist-packages/landscape/sysinfo/landscapelink.py
-sudo sed -i '/landscape.canonical.com/d' /usr/lib/python3/dist-packages/landscape/sysinfo/landscapelink.py
-sudo sed -i 's|self._sysinfo.add_footnote(|self._sysinfo.add_footnote("")|' /usr/lib/python3/dist-packages/landscape/sysinfo/landscapelink.py
-sudo sed -i '/printf/i \echo' /etc/update-motd.d/00-header
-sudo sed -i '/printf/d' /etc/update-motd.d/10-help-text
-sudo sed -Ezi.orig \
+sed -i 's|ENABLED=1|ENABLED=0|' /etc/default/motd-news
+sed -i '/Graph this data/d' /usr/lib/python3/dist-packages/landscape/sysinfo/landscapelink.py
+sed -i '/landscape.canonical.com/d' /usr/lib/python3/dist-packages/landscape/sysinfo/landscapelink.py
+sed -i 's|self._sysinfo.add_footnote(|self._sysinfo.add_footnote("")|' /usr/lib/python3/dist-packages/landscape/sysinfo/landscapelink.py
+sed -i '/printf/i \echo' /etc/update-motd.d/00-header
+sed -i '/printf/d' /etc/update-motd.d/10-help-text
+sed -Ezi.orig \
   -e 's/(def _output_esm_service_status.outstream, have_esm_service, service_type.:\n)/\1    return\n/' \
   -e 's/(def _output_esm_package_alert.*?\n.*?\n.:\n)/\1    return\n/' \
   /usr/lib/update-notifier/apt_check.py
-sudo /usr/lib/update-notifier/update-motd-updates-available --force
+/usr/lib/update-notifier/update-motd-updates-available --force
 
-# Customize login environment for sudo user
-sudo sed -i '44,54 s/^/#/' /etc/bash.bashrc
-sudo sed -i '38,64 s/^/#/' /home/noble/.bashrc
-sudo sed -i "66i\\\tPS1='\${debian_chroot:+(\$debian_chroot)}\\\[\\\033[01;31m\\\]\\\u\\\[\\\033[01;32m\\\]@\\\[\\\033[01;34m\\\]\\\h\\\[\\\033[00m\\\]:\\\[\\\033[01;32m\\\]\\\w\\\[\\\033[00m\\\]# '\n" /home/noble/.bashrc
-sudo sed -i "\$a\\\necho\nif [ -x /usr/games/fortune ]; then\n    /usr/games/fortune -s\nfi\necho\necho\necho -e \"\\\033[01;30m                 Server maintained by \\\033[01;34mClickwork\\\033[37m|\\\033[01;34mClockwork IT\\\033[37m\!\"\necho" /home/noble/.bashrc
+# Customize login environment for user
+echo -n "Customize bash & nano ........................... "
+sed -i '44,54 s/^/#/' /etc/bash.bashrc
+sed -i '38,64 s/^/#/' /home/noble/.bashrc
+sed -i "66i\\\tPS1='\${debian_chroot:+(\$debian_chroot)}\\\[\\\033[01;31m\\\]\\\u\\\[\\\033[01;32m\\\]@\\\[\\\033[01;34m\\\]\\\h\\\[\\\033[00m\\\]:\\\[\\\033[01;32m\\\]\\\w\\\[\\\033[00m\\\]# '\n" /home/noble/.bashrc
+sed -i "\$a\\\necho\nif [ -x /usr/games/fortune ]; then\n    /usr/games/fortune -s\nfi\necho\necho\necho -e \"\\\033[01;30m                 Server maintained by \\\033[01;34mClickwork\\\033[37m|\\\033[01;34mClockwork IT\\\033[37m\!\"\necho" /home/noble/.bashrc
 
 # Customize nanorc default text higlighting
-sudo cp -f "$scriptdir"/confs/env.default.nanorc /usr/share/nano/default.nanorc
+cp -f "$scriptdir"/confs/env.default.nanorc /usr/share/nano/default.nanorc
+echo -e "[\033[32m OK \033[0m]\n"
+
 
 # Download & Install CSF
+echo -n "Download and install CSF ........................ "
 cd /opt || { echo "Unable to change into /opt directory"; exit 1; }
-sudo wget https://download.configserver.com/csf.tgz
-sudo tar xzvf csf.tgz
+wget https://download.configserver.com/csf.tgz
+tar xzvf csf.tgz
 cd csf || { echo "Unable to change into /opt/csf directory"; exit 1; }
-sudo ./install.sh
+
+
+(while :; do for c in / - \\ \|; do printf '%s\b' "$c"; sleep 0.1; done; done) &
+./install.sh &> /dev/null || { echo -e "\n \033[1;91m[FAILED]\033[0m"; echo; exit 1; } ; { echo -e "[\033[32m OK \033[0m]\n"; kill $! && wait $!; } 2>/dev/null
 
 # temporarily disable firewall
-sudo csf -x
+echo -n "Disable firewall temporarily .................... "
+(while :; do for c in / - \\ \|; do printf '%s\b' "$c"; sleep 0.1; done; done) &
+csf -x &> /dev/null || { echo -e "\n \033[1;91m[FAILED]\033[0m"; echo; exit 1; } ; { echo -e "[\033[32m OK \033[0m]\n"; kill $! && wait $!; } 2>/dev/null
+
 
 hostname=$(hostname)
 
 # Configure CSF
-sudo sed -i 's|TESTING = "1"|TESTING = "0"|' /etc/csf/csf.conf
-sudo sed -i '/TCP_IN =/c\TCP_IN = "2282"' /etc/csf/csf.conf
-sudo sed -i '/TCP_OUT =/c\TCP_OUT = "20,21,25,53,80,113,443,2282,11371"' /etc/csf/csf.conf
-sudo sed -i '/UDP_IN =/c\UDP_IN = ""' /etc/csf/csf.conf
-sudo sed -i '/UDP_OUT =/c\UDP_OUT = "20,21,53,113,123"' /etc/csf/csf.conf
-sudo sed -i '/ICMP_IN =/c\ICMP_IN = "0"' /etc/csf/csf.conf
-sudo sed -i 's|IPV6 = "0"|IPV6 = "1"|' /etc/csf/csf.conf
-sudo sed -i '/TCP6_IN =/c\TCP6_IN = ""' /etc/csf/csf.conf
-sudo sed -i '/TCP6_OUT =/c\TCP6_OUT = ""' /etc/csf/csf.conf
-sudo sed -i '/UDP6_IN =/c\UDP6_IN = ""' /etc/csf/csf.conf
-sudo sed -i '/UDP6_OUT =/c\UDP6_OUT = ""' /etc/csf/csf.conf
-sudo sed -i '/LF_ALERT_TO =/c\LF_ALERT_TO = "alerts@clickwork.ro"' /etc/csf/csf.conf
-sudo sed -i "/LF_ALERT_FROM =/c\LF_ALERT_FROM = \"lfd@$hostname\"" /etc/csf/csf.conf
-sudo sed -i 's|RESTRICT_SYSLOG = "0"|RESTRICT_SYSLOG = "2"|' /etc/csf/csf.conf
-sudo sed -i 's|PS_INTERVAL = "0"|PS_INTERVAL = "60"|' /etc/csf/csf.conf
-sudo sed -i 's|PS_LIMIT = "10"|PS_LIMIT = "6"|' /etc/csf/csf.conf
-sudo sed -i 's|IPTABLES_LOG = "/var/log/messages"|IPTABLES_LOG = "/var/log/syslog"|' /etc/csf/csf.conf
-sudo sed -i 's|SYSLOG_LOG = "/var/log/messages"|SYSLOG_LOG = "/var/log/syslog"|' /etc/csf/csf.conf
-sudo sed -i 's|PS_PORTS = "0:65535,ICMP"|PS_PORTS = "0:65535,ICMP,BRD"|' /etc/csf/csf.conf
+echo -n "Configure CSF ................................... "
+
+sed -i 's|TESTING = "1"|TESTING = "0"|' /etc/csf/csf.conf
+sed -i '/TCP_IN =/c\TCP_IN = "2282"' /etc/csf/csf.conf
+sed -i '/TCP_OUT =/c\TCP_OUT = "20,21,25,53,80,113,443,2282,11371"' /etc/csf/csf.conf
+sed -i '/UDP_IN =/c\UDP_IN = ""' /etc/csf/csf.conf
+sed -i '/UDP_OUT =/c\UDP_OUT = "20,21,53,113,123"' /etc/csf/csf.conf
+sed -i '/ICMP_IN =/c\ICMP_IN = "0"' /etc/csf/csf.conf
+sed -i 's|IPV6 = "0"|IPV6 = "1"|' /etc/csf/csf.conf
+sed -i '/TCP6_IN =/c\TCP6_IN = ""' /etc/csf/csf.conf
+sed -i '/TCP6_OUT =/c\TCP6_OUT = ""' /etc/csf/csf.conf
+sed -i '/UDP6_IN =/c\UDP6_IN = ""' /etc/csf/csf.conf
+sed -i '/UDP6_OUT =/c\UDP6_OUT = ""' /etc/csf/csf.conf
+sed -i '/LF_ALERT_TO =/c\LF_ALERT_TO = "alerts@clickwork.ro"' /etc/csf/csf.conf
+sed -i "/LF_ALERT_FROM =/c\LF_ALERT_FROM = \"lfd@$hostname\"" /etc/csf/csf.conf
+sed -i 's|RESTRICT_SYSLOG = "0"|RESTRICT_SYSLOG = "2"|' /etc/csf/csf.conf
+sed -i 's|PS_INTERVAL = "0"|PS_INTERVAL = "60"|' /etc/csf/csf.conf
+sed -i 's|PS_LIMIT = "10"|PS_LIMIT = "6"|' /etc/csf/csf.conf
+sed -i 's|IPTABLES_LOG = "/var/log/messages"|IPTABLES_LOG = "/var/log/syslog"|' /etc/csf/csf.conf
+sed -i 's|SYSLOG_LOG = "/var/log/messages"|SYSLOG_LOG = "/var/log/syslog"|' /etc/csf/csf.conf
+sed -i 's|PS_PORTS = "0:65535,ICMP"|PS_PORTS = "0:65535,ICMP,BRD"|' /etc/csf/csf.conf
+echo -e "[\033[32m OK \033[0m]\n"
 
 # Configure CSF/LFD Exclusions
-sudo cat "$scriptdir"/snips/csf.pignore.snip | sudo tee --append /etc/csf/csf.pignore > /dev/null
+echo -n "Configure LFD exclusions ........................ "
+cat "$scriptdir"/snips/csf.pignore.snip >> /etc/csf/csf.pignore || { echo -e "\n \033[1;91m[FAILED]\033[0m"; exit 1; } ; echo -e "[\033[32m OK \033[0m]\n"
 
 # Copy firewall messages from syslog to firewall logfile
-sudo mkdir /var/log/csf 
-echo -e "# Log kernel generated firewall log to file\n:msg,contains,\"Firewall:\" /var/log/csf/csf.fw.log" | sudo tee /etc/rsyslog.d/22-firewall.conf > /dev/null
+echo -n "Create firewall log ............................. "
+mkdir /var/log/csf 
+echo -e "# Log kernel generated firewall log to file\n:msg,contains,\"Firewall:\" /var/log/csf/csf.fw.log" > /etc/rsyslog.d/22-firewall.conf || { echo -e "\n \033[1;91m[FAILED]\033[0m"; exit 1; } ; echo -e "[\033[32m OK \033[0m]\n"
 
 # logrotate firewall logs
+echo -n "Create logrotate for firewall logs .............. "
 echo -e '
-/var/log/csf/* {
+/var/log/csf/*.log {
 	daily
 	missingok
 	rotate 30
@@ -115,7 +131,7 @@ echo -e '
 	delaycompress
 	notifempty
 	create 640 syslog adm
-}' | sudo tee /etc/logrotate.d/csf > /dev/null
+}' > /etc/logrotate.d/csf || { echo -e "\n \033[1;91m[FAILED]\033[0m"; exit 1; } ; echo -e "[\033[32m OK \033[0m]\n"
 
 
 
@@ -123,15 +139,32 @@ echo -e '
 ## Last update & upgrade ##
 ###########################
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
+echo -n "Running update .................................. "
+(while :; do for c in / - \\ \|; do printf '%s\b' "$c"; sleep 0.1; done; done) &
+apt-get update &> /dev/null || { echo -e "\n \033[1;91m[FAILED]\033[0m"; echo; exit 1; } ; { echo -e "[\033[32m OK \033[0m]\n"; kill $! && wait $!; } 2>/dev/null
+
+echo -n "Running upgrades ................................ "
+(while :; do for c in / - \\ \|; do printf '%s\b' "$c"; sleep 0.1; done; done) &
+apt-get upgrade -y &> /dev/null || { echo -e "\n \033[1;91m[FAILED]\033[0m"; echo; exit 1; } ; { echo -e "[\033[32m OK \033[0m]\n"; kill $! && wait $!; } 2>/dev/null
+
+echo -n "Running more upgrades ........................... "
+(while :; do for c in / - \\ \|; do printf '%s\b' "$c"; sleep 0.1; done; done) &
+apt-get dist-upgrade -y &> /dev/null || { echo -e "\n \033[1;91m[FAILED]\033[0m"; echo; exit 1; } ; { echo -e "[\033[32m OK \033[0m]\n"; kill $! && wait $!; } 2>/dev/null
 
 
 
 ################################
 ## Enable firewall and reboot ##
 ################################
-sudo csf -e
-echo "You dungoofed! The server will self destruct!"
-sudo reboot
+
+echo -n "Enable firewall ................................. "
+(while :; do for c in / - \\ \|; do printf '%s\b' "$c"; sleep 0.1; done; done) &
+csf -e &> /dev/null || { echo -e "\n \033[1;91m[FAILED]\033[0m"; echo; exit 1; } ; { echo -e "[\033[32m OK \033[0m]\n"; kill $! && wait $!; } 2>/dev/null
+
+
+echo -n "Cleanup .......................................... "
+rm -rf "$scriptdir" || { echo -e "\n \033[1;91m[FAILED]\033[0m"; exit 1; } ; echo -e "[\033[32m OK \033[0m]\n"
+
+echo
+echo -e "Rebooting .................................... \033[32m-->"
+reboot
